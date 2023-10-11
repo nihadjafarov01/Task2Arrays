16a589de18df687fa9add0ff2c84c5d24a2d4180
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-10-11 05:55:04 +0400

    array task is done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 1, 7, 16, 5, 9 };
+            int[] newArray = new int[firstArray.Length];
+
+            for (int i = firstArray.Length - 1; i >= 0; i--)
+            {
+                newArray[firstArray.Length - 1 - i] = firstArray[i];
+            }
+
+            foreach (var item in newArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
